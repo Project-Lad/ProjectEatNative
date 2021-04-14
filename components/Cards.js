23e6df7c4a3e1499bf458abaadfaaff6b080d9d0
Darren Leh <lehdarren@gmail.com>
2021-04-14 17:17:14 -0400

    Yelp stars are now included.

--- components/Cards.js
+++ components/Cards.js
@@ -1,6 +1,28 @@
 import React, {useState} from 'react';
-import {Text, View, Button, Linking, Image, Modal, Pressable, StyleSheet} from "react-native";
+import {Text, View, Button, Linking, Image, Modal, Pressable, StyleSheet, Platform} from "react-native";
 import burger from '../assets/burger.jpg';
+import androidStar0 from '../assets/android/stars_regular_0.png'
+import androidStar1 from '../assets/android/stars_regular_1.png'
+import androidStar15 from '../assets/android/stars_regular_1_half.png'
+import androidStar2 from '../assets/android/stars_regular_2.png'
+import androidStar25 from '../assets/android/stars_regular_2_half.png'
+import androidStar3 from '../assets/android/stars_regular_3.png'
+import androidStar35 from '../assets/android/stars_regular_3_half.png'
+import androidStar4 from '../assets/android/stars_regular_4.png'
+import androidStar45 from '../assets/android/stars_regular_4_half.png'
+import androidStar5 from '../assets/android/stars_regular_5.png'
+
+import iosStar0 from '../assets/ios/regular_0.png'
+import iosStar1 from '../assets/ios/regular_1.png'
+import iosStar15 from '../assets/ios/regular_1_half.png'
+import iosStar2 from '../assets/ios/regular_2.png'
+import iosStar25 from '../assets/ios/regular_2_half.png'
+import iosStar3 from '../assets/ios/regular_3.png'
+import iosStar35 from '../assets/ios/regular_3_half.png'
+import iosStar4 from '../assets/ios/regular_4.png'
+import iosStar45 from '../assets/ios/regular_4_half.png'
+import iosStar5 from '../assets/ios/regular_5.png'
+
 import SwipeCards from "react-native-swipe-cards-deck";
 import firebase from "../firebase";
 import "firebase/firestore"
@@ -17,9 +39,18 @@ class Card extends React.Component {
         return (
             <View style={styles.card}>
                 <Image source={{uri: `${this.props.imageURL}`}} style={styles.cardImage}/>
+
                 <Text style={styles.cardsText}>{this.props.name}</Text>
-                <Text style={styles.cardsText}>{this.props.address}</Text>
-                <Button title="Find Place" className="btnMaps" onPress={() => Linking.openURL(this.props.googleURL)} target="_blank"/>
+
+                <View style={styles.yelpStars}>
+                    <Text style={styles.yelpText}>{(this.props.distance / 1609.3).toFixed(2)} mi.</Text>
+                    <Text style={styles.yelpText}>{this.props.address}</Text>
+
+                    <Image source={this.props.rating} />
+                    <Text style={styles.yelpText}>Based on {this.props.review_count} Reviews</Text>
+                </View>
+
+                <Text style={styles.cardsText}>{this.props.is_closed}</Text>
             </View>
         )
     }
@@ -29,11 +60,14 @@ const Cards = (props) => {
     let [resCounter, setCounter] = useState(0);
     let [modalVisible, setModalVisible] = useState(false);
     let [cardState, setCardState] = useState({
-        id: "12345",
+        id: "0",
         name: "name",
         price_range: "price_range",
         address: "address",
         rating: "rating",
+        review_count: "0",
+        distance: "0",
+        is_closed: "",
         phone_numbers: "phone_number",
         googleURL: "googleURL",
         imageURL: "imageURL"
@@ -47,9 +81,6 @@ const Cards = (props) => {
     for (let i = 0; i < props.restaurantData.length; i++) {
         const current = props.restaurantData[i];
 
-        //console.log(props)
-        //address = current.restaurant.location.address.split(' ');
-        //name = current.restaurant.name.split(' ');
         address = [current.location.address1, current.location.city, current.location.state];
         name = current.name.split(' ');
 
@@ -67,13 +98,6 @@ const Cards = (props) => {
             counter++;
         }
 
-        /*const id = current.restaurant.id;
-        name = current.restaurant.name;
-        const price_range = current.restaurant.price_range;
-        const rating = current.restaurant.user_rating.aggregate_rating;
-        address = current.restaurant.location.address;
-        const phone_numbers = current.restaurant.phone_numbers;*/
-
         const id = current.id;
         name = current.name;
         const price_range = current.price;
@@ -81,21 +105,96 @@ const Cards = (props) => {
         if(current.location.address2 !== '' && current.location.address2 !== 'null') {
             address += ', ';
             address += current.location.address2;
-            address += ', ';
         }
 
         if(current.location.address3 !== '' && current.location.address3 !== 'null') {
             address += ', ';
             address += current.location.address3;
-            address += ', ';
         }
 
         address += '\n';
 
         address += current.location.city + ', ' + current.location.state;
         const phone_number = current.display_phone;
-        const rating = current.rating;
+        let rating = current.rating;
         const imageURL = current.image_url;
+        const distance = current.distance;
+        const review_count = current.review_count;
+        let open_or_closed;
+
+        if(current.is_closed === true) {
+            open_or_closed = "Closed"
+        } else {
+            open_or_closed = "Open Now!"
+        }
+
+        if(Platform.OS === 'android') {
+            switch(rating) {
+                case 0:
+                    rating = androidStar0
+                    break;
+                case 1:
+                    rating = androidStar1
+                    break;
+                case 1.5:
+                    rating = androidStar15
+                    break;
+                case 2:
+                    rating = androidStar2
+                    break;
+                case 2.5:
+                    rating = androidStar25
+                    break;
+                case 3:
+                    rating = androidStar3
+                    break;
+                case 3.5:
+                    rating = androidStar35
+                    break;
+                case 4:
+                    rating = androidStar4
+                    break;
+                case 4.5:
+                    rating = androidStar45
+                    break;
+                case 5:
+                    rating = androidStar5
+                    break;
+            }
+        } else {
+            switch(rating) {
+                case 0:
+                    rating = iosStar0
+                    break;
+                case 1:
+                    rating = iosStar1
+                    break;
+                case 1.5:
+                    rating = iosStar15
+                    break;
+                case 2:
+                    rating = iosStar2
+                    break;
+                case 2.5:
+                    rating = iosStar25
+                    break;
+                case 3:
+                    rating = iosStar3
+                    break;
+                case 3.5:
+                    rating = iosStar35
+                    break;
+                case 4:
+                    rating = iosStar4
+                    break;
+                case 4.5:
+                    rating = iosStar45
+                    break;
+                case 5:
+                    rating = iosStar5
+                    break;
+            }
+        }
 
         data.push({
             id: id,
@@ -103,6 +202,9 @@ const Cards = (props) => {
             price_range: price_range,
             address: address,
             rating: rating,
+            review_count: review_count,
+            distance: distance,
+            is_closed: open_or_closed,
             phone_numbers: phone_number,
             googleURL: googleURL,
             imageURL: imageURL
@@ -174,7 +276,6 @@ const Cards = (props) => {
             <View style={styles.container}>
                 <Modal
                     animationType="slide"
-                    transparent={true}
                     visible={modalVisible}
                     onRequestClose={() => {
                         setModalVisible(!modalVisible);
@@ -222,18 +323,27 @@ const styles = StyleSheet.create({
         borderWidth: 2,
     },
     card: {
-        flex: 0.98,
+        flex: 1,
         backgroundColor: "#bc0402",
-        alignItems: "center",
-        justifyContent: "center",
         borderRadius:10,
         borderWidth: 2,
         borderColor: '#20232a'
     },
     cardsText: {
-        fontSize: 25,
-        fontFamily: 'Capriola_400Regular',
+        fontSize: 20,
+        fontWeight: "bold",
+        alignSelf: 'center',
         color: '#010001'
+    },
+    yelpText: {
+        fontSize: 15,
+        color: '#010001'
+    },
+    yelpStars: {
+        paddingStart: 10,
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start',
+        flexDirection: 'column',
     },
     cardImage: {
         width: "65%",
