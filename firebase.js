214b098bd98ffa21054346f305495292fb3042b6
Darren Leh <lehdarren@gmail.com>
2024-03-27 20:46:27 -0400

    - added back auto-login

--- firebase.js
+++ firebase.js
@@ -1,6 +1,8 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import {API_KEY, AUTH_DOMAIN, DATABASE_URL, STORAGE_BUCKET, MESSAGE_SENDER_ID, APP_ID} from '@env'
+import { initializeAuth, getReactNativePersistence } from 'firebase/auth';
+import ReactNativeAsyncStorage from '@react-native-async-storage/async-storage';
 
 const app = initializeApp({
     apiKey:API_KEY,
@@ -10,7 +12,10 @@ const app = initializeApp({
     messagingSenderId: MESSAGE_SENDER_ID,
     appId: APP_ID
 });
-const auth = getAuth(app);
+
+const auth = initializeAuth(app, {
+    persistence: getReactNativePersistence(ReactNativeAsyncStorage)
+});
 //export const db = getFirestore();
 export default app
 
